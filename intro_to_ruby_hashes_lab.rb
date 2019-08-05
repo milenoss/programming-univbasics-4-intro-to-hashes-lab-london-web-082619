1d9a7eacf01b64d77321159fa200c3bd92295b2a
Milen Khanal <milenkhanal@gmail.com>
2019-08-05 17:00:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {:key => value}
+  hash = {:name => 'Grace Hopper'}
 end
 
 def read_from_hash(hash, key)
